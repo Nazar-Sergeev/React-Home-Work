5c7f601816703a41d38503f26fcdeb79140a6357
Nazar-Sergeev <privat.nazar@gmail.com>
2021-06-26 22:11:10 +0300

    Home Work1 part2

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -4,22 +4,22 @@ export default function Users() {
 
     let [usersList, setUsersList] = useState([]);
 
-    // let usersList = state[0];
-    // let setUsersList = state[1];
-
-useEffect(() => {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
-                .then(value => value.json())
-                .then(response => {
-                    setUsersList(response);
-                });
-}, []);
+            .then(value => value.json())
+            .then(value => {
+                setUsersList(value);
+            });
+
+    });
+
     return (
         <div>
             {
-                usersList.map(value => <h2>{value.id} - {value.name}</h2>)
+                usersList.map(value => <div>{value.id} - {value.name}
+                <button>Click Me</button>
+                </div>)
             }
         </div>
     );
 }
-
